d83b194d430a9383e972d624ad1bad81d28a2152
irenenyatichi <irenenyatichik@gmail.com>
2021-05-06 09:57:36 +0300

    first commit

---
+++ Assesment.py
@@ -0,0 +1,21 @@
+a = {6,7,8,9,10}
+b ={5,6,7,8,9}
+a.add(4)
+b.add(3)
+print(a & b)
+print(a-b)
+
+lst = [11, 100, 99, 1000, 999, 99]
+print(lst[1])
+print(lst[-1])
+print([])
+
+y = range[2020 , 2070]
+for a in y:
+    if a % 5 == 0:
+        print(y)
+
+z = range[1000 , 2000]
+for b in z:
+    if b/7 == 0 && b%5 !=0:
+        print(b)
